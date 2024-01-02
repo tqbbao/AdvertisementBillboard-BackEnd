5c8aa335ecbe58c98af5990c8a92995481d480af
Bao Tran <tqbbao@gmail.com>
2024-01-02 13:59:36 +0700

    [#29].

--- src/reverse-geocoding/reverse-geocoding.service.ts
+++ src/reverse-geocoding/reverse-geocoding.service.ts
@@ -20,37 +20,37 @@ export class ReverseGeocodingService {
 
     const resultGeoCoding = (await dataGeocoding).data;
     // //Latitude and longitude
-    // const [longitude, latitude] = resultGeoCoding.query;
-    // //Ward 
-    // const wardId = await this.wardsService.findByIdGeo(resultGeoCoding.features[1].id);
-    // const ward = {
-    //   id: wardId[0].id,
-    //   name: resultGeoCoding.features[1].text,
-    // };
-    // //District
-    // const districtId = await this.districtsService.findByIdGeo(resultGeoCoding.features[3].id);
-    // const district = {
-    //   id: districtId[0].id,
-    //   name: resultGeoCoding.features[3].text,
-    // };
-    // //Address
-    // const address = resultGeoCoding.features[0].properties.address? resultGeoCoding.features[0].properties.address : resultGeoCoding.features[0].place_name;
+    const [longitude, latitude] = resultGeoCoding.query;
+    //Ward 
+    const wardId = await this.wardsService.findByIdGeo(resultGeoCoding.features[1].id);
+    const ward = {
+      id: wardId[0].id,
+      name: resultGeoCoding.features[1].text,
+    };
+    //District
+    const districtId = await this.districtsService.findByIdGeo(resultGeoCoding.features[3].id);
+    const district = {
+      id: districtId[0].id,
+      name: resultGeoCoding.features[3].text,
+    };
+    //Address
+    const address = resultGeoCoding.features[0].properties.address? resultGeoCoding.features[0].properties.address : resultGeoCoding.features[0].place_name;
 
-    // //Full address
-    // const fullAddress = `${address}, ${ward.name}, ${district.name}`;
+    //Full address
+    const fullAddress = `${address}, ${ward.name}, ${district.name}`;
 
     return {
-      phuong: resultGeoCoding.features[1].id,
-      quan: resultGeoCoding.features[3].id,
-      namephuong: resultGeoCoding.features[1].text,
-      namequan: resultGeoCoding.features[3].text,
+      // phuong: resultGeoCoding.features[1].id,
+      // quan: resultGeoCoding.features[3].id,
+      // namephuong: resultGeoCoding.features[1].text,
+      // namequan: resultGeoCoding.features[3].text,
 
-      // latitude,
-      // longitude,
-      // ward,
-      // district,
-      // address,
-      // fullAddress,
+      latitude,
+      longitude,
+      ward,
+      district,
+      address,
+      fullAddress,
     }
   }
 }
