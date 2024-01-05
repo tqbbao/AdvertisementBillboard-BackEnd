ae28484993d2b10c188f401258ab238d35930a86
Bao Tran <tqbbao@gmail.com>
2024-01-05 17:48:05 +0700

    [#35] update api sendEmail

--- src/request-space/request-space.service.ts
+++ src/request-space/request-space.service.ts
@@ -9,7 +9,8 @@ import { MailerService } from '@nestjs-modules/mailer';
 import { ReportsSpaceService } from 'src/reports-space/reports-space.service';
 import { SpacesService } from 'src/spaces/spaces.service';
 import { PaginationRequestSpace } from './dto/pagination';
-
+import * as path from 'path';
+import * as fs from 'fs';
 @Injectable()
 export class RequestSpaceService {
   constructor(
@@ -39,7 +40,37 @@ export class RequestSpaceService {
       await this.reportsSpaceService.updateStateReportSpace(
         parseInt(String(data.reportSpace)),
       );
-      return await this.requestEditSpaceRepository.save(requestEditSpace);
+
+      //send mail to user (processing)
+      const reportSpace = await this.reportsSpaceService.findReportSpaceById(
+        parseInt(String(data.reportSpace)),
+      );
+
+      const requestSpace = await this.requestEditSpaceRepository.save(requestEditSpace);
+
+      const emailData = {
+        email: reportSpace.email,
+        name: reportSpace.name,
+        state: reportSpace.state,
+      };
+      const htmlFilePath = path.join(
+        __dirname,
+        '../../src/templates/email/processingEmail.html',
+      );
+      let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+      for (const key in emailData) {
+        const regex = new RegExp(`{{${key}}}`, 'g');
+        htmlContent = htmlContent.replace(regex, emailData[key]);
+      }
+
+      await this.mailerService.sendMail({
+        to: `${emailData.email}`, // Địa chỉ email người nhận
+        subject: 'Report the results you provided', // Tiêu đề của email
+        html: htmlContent,
+      });
+
+
+      return requestSpace;
     } catch (error) {
       throw error;
     }
@@ -166,17 +197,29 @@ export class RequestSpaceService {
         state: reportSpace.state,
       };
 
+
+      const htmlFilePath = path.join(
+        __dirname,
+        '../../src/templates/email/processedEmail.html',
+      );
+      let htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+      for (const key in emailData) {
+        const regex = new RegExp(`{{${key}}}`, 'g');
+        htmlContent = htmlContent.replace(regex, emailData[key]);
+      }
+
       await this.mailerService.sendMail({
         to: `${emailData.email}`, // Địa chỉ email người nhận
         subject: 'Report the results you provided', // Tiêu đề của email
-        html: `<p>Dear ${emailData.name},</p>
-        <p>You have selected <a href="mailto:xxxxx@gmail.com">xxxxx@gmail.com</a> as your new Apple ID email address. To verify this email address belongs to you, enter the code below on the Apple ID website, verification page:</p>
-        <p><strong>${emailData.state}</strong></p>
-        <p>This code will expire three hours after this email was sent.</p>
-        <p><em>Why you received this email.</em><br/>
-        Apple requires verification whenever an email address is selected as an Apple ID.</p>
-        <p>If you did not make this request, you can ignore this email or report it to Apple Support.</p>
-        `, // Nội dung của email (HTML)
+        // html: `<p>Dear ${emailData.name},</p>
+        // <p>You have selected <a href="mailto:xxxxx@gmail.com">xxxxx@gmail.com</a> as your new Apple ID email address. To verify this email address belongs to you, enter the code below on the Apple ID website, verification page:</p>
+        // <p><strong>${emailData.state}</strong></p>
+        // <p>This code will expire three hours after this email was sent.</p>
+        // <p><em>Why you received this email.</em><br/>
+        // Apple requires verification whenever an email address is selected as an Apple ID.</p>
+        // <p>If you did not make this request, you can ignore this email or report it to Apple Support.</p>
+        // `, // Nội dung của email (HTML)
+        html: htmlContent,
       });
     } catch (error) {
       throw error;
