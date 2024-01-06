eb142d2036b2619fd11ed9ea9ec9ef02bfa6069c
hagiang2906 <hana.hagiang@gmail.com>
2024-01-06 23:10:06 +0700

    [giang] swagger location-types

--- src/location-types/location-types.controller.ts
+++ src/location-types/location-types.controller.ts
@@ -1,4 +1,4 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateLocationTypesDto } from './dto/create-location-types.dto';
 import { LocationTypesService } from './location-types.service';
 import {
@@ -26,31 +26,51 @@ export class LocationTypesController {
   constructor(private locationTypesService: LocationTypesService) {}
 
   //Find all
+  @HttpCode(200)
   @Get()
+  @ApiOperation({ summary: 'Get all location types' })
+  @ApiResponse({ status: 200, description: 'Found all location types' })
   async findAll() {
     return await this.locationTypesService.findAll();
   }
 
+  @HttpCode(200)
   //Find by id
   @Get(':id')
+  @ApiOperation({ summary: 'Get a location type by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the location type' })
+  @ApiResponse({ status: 200, description: 'Found location type by ID' })
   async findById(@Param('id') id: number) {
-    return await this.locationTypesService.findById(id);
+    return await this.locationTypesService.findById(id)
+;
   }
 
   // Create
   @Post()
+  @ApiOperation({ summary: 'Update a location type by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the location type to update' })
+  @ApiBody({ type: CreateLocationTypesDto })
+  @ApiResponse({ status: 200, description: 'Location type updated successfully'})
   async create(@Body() data: CreateLocationTypesDto) {
     return await this.locationTypesService.create(data);
   }
 
   //Update
   @Put(':id')
+  @ApiOperation({ summary: 'Update a location type by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the location type to update' })
+  @ApiBody({ type: CreateLocationTypesDto })
+  @ApiResponse({ status: 200, description: 'Location type updated successfully' })
   async update(@Param('id') id: number, @Body() data: CreateLocationTypesDto) {
     return await this.locationTypesService.update(id, data);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete a location type by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the location type to delete' })
+  @ApiResponse({ status: 200, description: 'Location type deleted successfully' })
   async remove(@Param('id') id: number) {
-    return await this.locationTypesService.remove(id);
+    return await this.locationTypesService.remove(id)
+;
   }
 }
