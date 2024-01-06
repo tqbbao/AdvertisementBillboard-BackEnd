661a566da5441aec18865e79ea0e9a7d71895ea0
hagiang2906 <hana.hagiang@gmail.com>
2024-01-06 21:52:01 +0700

    [giang] swagger form-advertising

--- src/form-advertising/dto/create-form-advertising.dto.ts
+++ src/form-advertising/dto/create-form-advertising.dto.ts
@@ -1,6 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class CreateFormAdvertisingDto {
+  @ApiProperty({ description: 'The name of the form advertising', required: true })
+
   @IsNotEmpty()
   name: string;
 }
--- src/form-advertising/form-advertising.controller.ts
+++ src/form-advertising/form-advertising.controller.ts
@@ -1,4 +1,4 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CreateFormAdvertisingDto } from './dto/create-form-advertising.dto';
 import { FormAdvertisingService } from './form-advertising.service';
 import {
@@ -14,6 +14,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Http } from 'winston/lib/winston/transports';
 
 @ApiTags('form-advertising')
 @Controller('form-advertising')
@@ -21,33 +22,57 @@ export class FormAdvertisingController {
   constructor(private formAdvertisingService: FormAdvertisingService) {}
 
   //Find all
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Danh sách hình thức quảng cáo' })
+  @ApiResponse({ status: 200, description: 'Danh sách hình thức quảng cáo' })
   @Get()
   async findAll() {
     return await this.formAdvertisingService.findAll();
   }
 
   //Find by id
+  @HttpCode(200)
+  @ApiOperation({ summary: 'Danh sách hình thức quảng cáo theo id' })
+  @ApiParam({ name: 'id', description: 'id hình thức quảng cáo', required: true })
+  @ApiResponse({ status: 200, description: 'Danh sách hình thức quảng cáo theo id' })
   @Get(':id')
   async findById(@Param('id') id: number) {
-    return await this.formAdvertisingService.findById(id);
+    return await this.formAdvertisingService.findById(id)
+;
   }
 
+
   // Create
+  @HttpCode(201)
+  @ApiOperation({ summary: 'Create a new form advertising' })
+  @ApiBody({ type: CreateFormAdvertisingDto })
+  @ApiResponse({ status: 201, description: 'Form advertising created successfully' })
   @Post()
   async create(@Body() data: CreateFormAdvertisingDto) {
     return await this.formAdvertisingService.create(data);
   }
 
+
   //Update
+
+  @ApiOperation({ summary: 'Update a form advertising by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the form advertising to update' })
+  @ApiBody({ type: CreateFormAdvertisingDto })
+  @ApiResponse({ status: 200, description: 'Form advertising updated successfully' })
   @Put(':id')
   async update(@Param('id') id: number, @Body() data: CreateFormAdvertisingDto) {
     return await this.formAdvertisingService.update(id, data);
   }
 
+
+
+  @ApiOperation({ summary: 'Delete a form advertising by ID' })
+  @ApiParam({ name: 'id', description: 'ID of the form advertising to delete' })
+  @ApiResponse({ status: 200, description: 'Form advertising deleted successfully' })
   @Delete(':id')
   async remove(@Param('id') id: number) {
-    return await this.formAdvertisingService.remove(id);
+    return await this.formAdvertisingService.remove(id)
+;
   }
-
 
 }
