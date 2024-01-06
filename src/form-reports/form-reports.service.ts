e2733391a4ae70c83135d04140633273a3759e98
Pham Duy Tung <22424034@student.hcmus.edu.vn>
2024-01-06 07:29:28 +0000

    Add CRUD operations for FormReportsController and FormReportsService

--- src/form-reports/form-reports.service.ts
+++ src/form-reports/form-reports.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { FormReport } from 'src/entity/form-report.entity';
 import { Repository } from 'typeorm';
+import { CreateFormReportsDto } from './dto/create-form-reports.dto';
 
 @Injectable()
 export class FormReportsService {
@@ -13,5 +14,34 @@ export class FormReportsService {
   //find all form reports
   async findAll() {
     return await this.formReportRepository.find();
+  }
+
+  // Find by id
+  async findById(id: number) {
+    return await this.formReportRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+
+  // Create
+  async create(data: CreateFormReportsDto) {
+    try {
+      const formAdvertising = await this.formReportRepository.create(data);
+      return await this.formReportRepository.save(formAdvertising);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //Update
+  async update(id: number, data: CreateFormReportsDto) {
+    return await this.formReportRepository.update(id, data);
+  }
+
+  // soft Delete
+  async remove(id: number) {
+    return await this.formReportRepository.softDelete(id);
   }
 }
